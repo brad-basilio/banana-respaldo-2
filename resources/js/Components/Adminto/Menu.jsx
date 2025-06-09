715790e56a5722a474444e3aa9c9175b98ff93e7
Papayadev2024 <diegomartinez.papaya@gmail.com>
2025-06-09 09:37:25 -0500

    final

--- resources/js/Components/Adminto/Menu.jsx
+++ resources/js/Components/Adminto/Menu.jsx
@@ -125,7 +125,7 @@ const Menu = ({ session, hasRole }) => {
                   <MenuItem href="/admin/strengths" icon="mdi mdi-arm-flex">Fortalezas</MenuItem>
                   <MenuItem href="/admin/banners" icon="mdi mdi-post-outline">Banners</MenuItem>
                   {/* <MenuItem href="/admin/sliders" icon="mdi mdi-page-layout-body">Sliders</MenuItem> */}
-                  <MenuItem href="/admin/coupons" icon="mdi mdi-page-layout-body">Cupones</MenuItem>
+                  {/* <MenuItem href="/admin/coupons" icon="mdi mdi-page-layout-body">Cupones</MenuItem> */}
                   {/* <MenuItem href="/admin/faqs" icon="mdi mdi-forum">Preguntas Frecuentes</MenuItem> */}
                   {/* <MenuItem href="/admin/testimonies" icon="mdi mdi-forum">Testimonios</MenuItem> */}
                   <MenuItem href="/admin/socials" icon="mdi mdi-web">Redes Sociales</MenuItem>
