b7844744890550237ba6444f6d2ed712d048a927
brad-basilio <brad.basilioppy@gmail.com>
2025-06-18 19:48:14 -0500

    version final

--- resources/js/Components/Tailwind/Products/ProductInfinite.jsx
+++ resources/js/Components/Tailwind/Products/ProductInfinite.jsx
@@ -66,7 +66,7 @@ const ProductInfinite = ({ items, data, setCart, cart }) => {
                 </div>
 
                 {/* Swiper Carousel */}
-                <div className="relative px-10">
+                <div className="relative lg:px-10">
                     <Swiper
                         modules={[Navigation, Grid]}
                         navigation={{
