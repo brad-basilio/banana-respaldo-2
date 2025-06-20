2f91c5ea4ad0c9e137aa86e248829ef279d7a37c
brad-basilio <brad.basilioppy@gmail.com>
2025-06-20 09:23:40 -0500

    mejora

--- resources/js/Components/Tailwind/Checkouts/Components/ShippingStep.jsx
+++ resources/js/Components/Tailwind/Checkouts/Components/ShippingStep.jsx
@@ -973,7 +973,7 @@ export default function ShippingStep({
                             {appliedCoupon && (
                                 <div className="bg-gray-50 border-2  rounded-xl p-4">
                                     <div className="flex items-center justify-between">
-                                        <div className="flex items-center gap-3">
+                                        <div className="flex items-center gap-3 w-8/12">
                                             <div className="w-8 h-8 bg-primary rounded-full flex items-center justify-center flex-shrink-0">
                                                 <svg className="w-4 h-4 text-white" fill="currentColor" viewBox="0 0 20 20">
                                                     <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
@@ -988,8 +988,8 @@ export default function ShippingStep({
                                                 </p> */}
                                             </div>
                                         </div>
-                                        <div className="text-right">
-                                            <span className="customtext-primary font-bold text-lg">
+                                        <div className="text-right w-4/12">
+                                            <span className="customtext-primary font-bold text-base">
                                                 -S/ {Number2Currency(roundToTwoDecimals(couponDiscount))}
                                             </span>
                                            {/* <p className="customtext-primary text-xs">Descuento aplicado</p> */}
