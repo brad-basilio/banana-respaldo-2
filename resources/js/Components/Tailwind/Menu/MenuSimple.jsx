7b779d91dfbfe91aabf70fd92cad6c7724003b7d
brad-basilio <brad.basilioppy@gmail.com>
2025-06-28 10:51:13 -0500

    add

--- resources/js/Components/Tailwind/Menu/MenuSimple.jsx
+++ resources/js/Components/Tailwind/Menu/MenuSimple.jsx
@@ -115,7 +115,7 @@ const MenuSimple = ({ pages = [], items, data }) => {
                                     }
                                     title={tag.description || tag.name}
                                 >
-                                    <Tag size={16} className="text-primary" />
+                                    <Tag size={16} className="text-white" />
                                     {tag.name}
                                     {/* <span className="text-xs text-gray-500 bg-gray-100 px-2 py-1 rounded-full">
                                     {tag.items_count}
