94bdf00f9021504fab5fe8cd77c025c585d8ab14
brad-basilio <brad.basilioppy@gmail.com>
2025-06-30 13:04:45 -0500

    add

--- resources/views/public_backup.blade.php
+++ resources/views/public_backup.blade.php
@@ -15,7 +15,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{ $data['name'] ?? 'Página' }} | {{ env('APP_NAME', 'Sala Fabulosa') }}</title>
+    <title>{{ $data['name'] ?? 'Página' }} | {{ env('APP_NAME') }}</title>
 
     <link rel="shortcut icon" href="/assets/resources/icon.png?v={{ uniqid() }}" type="image/png">
 
